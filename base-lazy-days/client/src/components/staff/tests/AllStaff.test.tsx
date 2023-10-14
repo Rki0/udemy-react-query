fdacef07d8828ac7954b641e22f0f8484235245e
Rki0 <pky00823@gmail.com>
2023-10-14 10:28:01 +0900

    basic of react-query

---
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react';
+
+import { rest } from 'msw';
+// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
+import { server } from '../../../mocks/server';
+// import { renderWithClient } from '../../../test-utils';
+import { AllStaff } from '../AllStaff';
+import { renderWithQueryClient } from 'test-utils';
+
+test('renders response from query', async () => {
+  // write test here
+  renderWithQueryClient(<AllStaff />);
+
+  const staffNames = await screen.findAllByRole('heading', {
+    name: /divya|sandra|michael|mateo/i,
+  });
+
+  expect(staffNames).toHaveLength(4);
+});
+
+test('handles query error', async () => {
+  // (re)set handler to return a 500 error for staff
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+});
---
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -0,0 +1,39 @@
+import { RenderResult, render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider, setLogger } from 'react-query';
+
+import { defaultQueryClientOptions } from '../react-query/queryClient';
+import { ReactElement } from 'react';
+import { generateQueryClient } from '../react-query/queryClient';
+
+setLogger({
+  log: console.log,
+  warn: console.warn,
+  error: () => {},
+});
+
+const generateTestQueryClient = () => {
+  // return new QueryClient();
+  const client = generateQueryClient();
+  const options = client.getDefaultOptions();
+  options.queries = { ...options.queries, retry: false };
+  return client;
+};
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  const queryClient = client ?? generateTestQueryClient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
+// // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
+// export const createQueryClientWrapper = (): React.FC => {
+//   const queryClient = generateTestQueryClient();
+//   return ({ children }) => (
+//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+//   );
+// };
